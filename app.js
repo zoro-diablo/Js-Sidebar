c5ccf16d3d0ccd781b0328966d3abfda7ffdde07
zorodiablo <vyshnavk117@gmail.com>
2022-12-15 15:40:42 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+const toggleBtn=document.querySelector('.sidebar-toggle');
+const closeBtn=document.querySelector('.close-btn');
+const sideBar=document.querySelector('.sidebar');
+
+toggleBtn.addEventListener('click',function(){
+    sideBar.classList.toggle('show-sidebar');
+});
+closeBtn.addEventListener('click',function(){
+    sideBar.classList.toggle('show-sidebar');
+});
